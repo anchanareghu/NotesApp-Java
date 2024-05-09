85fbf63a978cd7e771e6cbf30bf919d5102d51f7
Anchana <anchanareghu20@gmail.com>
2024-05-09 21:38:54 +0530

    push files to git

--- app/src/main/java/com/example/notes/Note.java
+++ app/src/main/java/com/example/notes/Note.java
@@ -3,7 +3,6 @@ package com.example.notes;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-
 @Entity(tableName = "notes")
 public class Note {
     @PrimaryKey(autoGenerate = true)
